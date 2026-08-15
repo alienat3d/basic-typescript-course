38250dd9f372a60a14bc2de81bc8afbee9aff476
Al Zaplin <zaplin.dev@gmail.com>
2026-08-15 06:21:21 +0300

    Add(code & notes): For Lesson #94: The rules of decorator evaluation and show on example the order in which the decorators are initialized and called

---
+++ 94-decorators-evaluation.ts
@@ -0,0 +1,166 @@
+import "reflect-metadata";
+
+const limitMetadataKey = Symbol("limit");
+
+interface ICar {
+	fuel: string;
+	open: boolean;
+	freeSeats: number;
+}
+
+@changeDoorStatus(false)
+@changeAmountOfFuel(95)
+class myCar implements ICar {
+	fuel: string = "50%";
+	open: boolean = true;
+	test: any;
+
+  // ? 94.2.1 А ещё, в документации сказано, что декораторы параметров могут являться конструктором. Ведь конструктор это по сути обычный метод, у которого есть параметры и их мы можем декорировать.
+	constructor(@limit() test: number) {
+		this.test = test;
+	}
+
+	@checkNumberOfSeats(4)
+	freeSeats: number;
+
+	@checkAmountOfFuel()
+	isOpen(value: string) {
+		return this.open ? "open" : `close ${value}`;
+	}
+
+	@validate()
+	startTravel(@limit() passengers: number) {
+		console.log(`Started with ${passengers} passengers`);
+	}
+}
+
+// ? 94.1 Итак, рассмотрим порядок выполнения декораторов на уже привычном примере класса автомобиля с различными декораторами, привязанными к нему. Мы добавили сюда выводы в консоль, чтобы точно увидеть, когда каждый декоратор инициализируется, а когда выполняется. Однако, тут нужно уточнить, что здесь можно было бы добавить ещё несколько вещей в это логирование, а именно декораторы статичных свойств (которые, кстати, инициализируются и вызываются после обычных свойств и методов класса) или методов класса, а ещё декораторы аксессоров.
+/* ? 94.2.0 Итак, нажмём выполнения кода и рассмотрим что же происходит в действительности с выполнением декораторов:
+* Init: Property Decorator // сначала инициализируется декоратор свойств ("checkNumberOfSeats") и т.к. у него нет цепочки, связанной с запуском других декораторов, то он сразу идёт в работу — "Call" на след. строчке
+* Call: Property Decorator
+* Init: Method Decorator // далее идёт инициализация декоратора метода ("validate")
+* Init: Parameter Decorator // сразу за декоратором метода идёт инициализация декоратора параметров этого же метода, по цепочке
+* Call: Parameter Decorator // затем идёт вызов сначала декоратора параметров...
+* Call: Method Decorator // ...а за ним уже декоратора метода (вспоминаем принцип вызова функции внутри другой функции, где инициализация функций идёт снаружи внутрь, а запуск наоборот изнутри наружу)
+* Init: Class Decorator Door // обратите внимание, что здесь идёт инициализация сразу двух декораторов класса и первым идёт верхний декоратор (т.е. который будет первым считан интерпретатором кода)
+* Init: Class Decorator Fuel
+* Init: Parameter Decorator // затем идёт инициализация декоратора параметра внутри конструктора...
+* Call: Parameter Decorator // ...а затем сразу его вызов
+* Call: Class Decorator Fuel // ну, и в самом конце, вызываются декораторы класса, начиная с самого внутреннего (иди нижнего в коде)...
+* Call: Class Decorator Door // ...и заканчивая самым внешним (самым верхним в коде)
+↑ */
+function limit() {
+	console.log("Init: Parameter Decorator");
+	return (
+		target: Object,
+		propertyKey: string | symbol,
+		parameterIndex: number
+	) => {
+		console.log("Call: Parameter Decorator");
+		let limitedParameters: number[] =
+			Reflect.getOwnMetadata(limitMetadataKey, target, propertyKey) || [];
+		limitedParameters.push(parameterIndex);
+		Reflect.defineMetadata(
+			limitMetadataKey,
+			limitedParameters,
+			target,
+			propertyKey
+		);
+	};
+}
+
+function validate() {
+	console.log("Init: Method Decorator");
+	return (
+		target: Object,
+		propertyKey: string | symbol,
+		descriptor: PropertyDescriptor
+	) => {
+		console.log("Call: Method Decorator");
+		let method = descriptor.value;
+
+		descriptor.value = function (...args: any) {
+			let limitedParameters: number[] = Reflect.getOwnMetadata(
+				limitMetadataKey,
+				target,
+				propertyKey
+			);
+
+			if (limitedParameters) {
+				for (let index of limitedParameters) {
+					if (args[index] > 4) {
+						throw new Error("Нельзя больше 4х пассажиров");
+					}
+				}
+			}
+			return method?.apply(this, args);
+		};
+	};
+}
+
+function checkNumberOfSeats(limit: number) {
+	console.log("Init: Property Decorator");
+	return function (target: Object, propertyKey: string | symbol) {
+		console.log("Call: Property Decorator");
+		let symbol = Symbol();
+
+		const getter = function (this: any) {
+			return this[symbol];
+		};
+
+		const setter = function (this: any, newAmount: number) {
+			if (newAmount >= 1 && newAmount < limit) {
+				this[symbol] = newAmount + 1;
+			} else {
+				// console.log(`Больше ${limit} сидений быть не может`);
+				Object.defineProperty(target, "errors", {
+					value: `Больше ${limit} сидений быть не может`,
+				});
+			}
+		};
+
+		Object.defineProperty(target, propertyKey, {
+			get: getter,
+			set: setter,
+		});
+	};
+}
+
+function checkAmountOfFuel() {
+	console.log("Init: Method Decorator");
+	return (
+		target: Object,
+		propertyKey: string | symbol,
+		descriptor: PropertyDescriptor
+	): PropertyDescriptor | void => {
+		console.log("Call: Method Decorator");
+		const oldValue = descriptor.value;
+		descriptor.value = function (this: any, ...args: any[]) {
+			console.log(this.fuel);
+			return oldValue.apply(this, args);
+		};
+	};
+}
+
+function changeDoorStatus(status: boolean) {
+	console.log("Init: Class Decorator Door");
+	return <T extends { new (...args: any[]): {} }>(constructor: T) => {
+		console.log("Call: Class Decorator Door");
+		return class extends constructor {
+			open = status;
+		};
+	};
+}
+
+function changeAmountOfFuel(amount: number) {
+	console.log("Init: Class Decorator Fuel");
+	return <T extends { new (...args: any[]): {} }>(constructor: T) => {
+		console.log("Call: Class Decorator Fuel");
+		return class extends constructor {
+			fuel = `${amount}%`;
+		};
+	};
+}
+
+const car = new myCar(3);
+car.startTravel(3);
---
+++ notes/94-decorators-evaluation.md
@@ -0,0 +1,10 @@
+*[Порядок декораторов ("decorators evaluation") (94)]*
+
+? 94.0 Здесь мы рассмотрим тему "В каком порядке выполняются все типы декораторов?". В некоторых прошлых уроках мы уже коснулись этой темы и говорили о том, что есть две фазы: инициализация и реализация.
+
+Рассмотрим тему в файле с примерами: [94-decorators-evaluation.ts]
+
+|===:===:===:===>
+**links**
+
+* (EN TS Docs "Decorator evaluation"): https://www.typescriptlang.org/docs/handbook/decorators.html#decorator-evaluation
